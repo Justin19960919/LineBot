89eadb3b651e6b421c2cbdb797dfedac31bf5a54
Justin Lee <jclee1996@gmail.com>
2021-04-22 23:25:35 -0700

    update rewritten code to git

--- ig.py
+++ ig.py
@@ -1,98 +1,122 @@
 import json
 import requests
-from lxml import etree
+import pandas as pd
+import datetime
+import os
+import time
 
-# Reference
-# https://dev.to/iankerins/the-easy-way-to-build-an-instagram-spider-using-python-scrapy-graphql-4gko
+# Using Rapid API to fetch instagram posts
+class getPosts:
 
-## meme library
-everday_a_meme = "https://www.instagram.com/everyday.a.meme/"
-dank_meme_therapy = "https://www.instagram.com/dank_meme_therapy/"
-gokusanmemes = "https://www.instagram.com/gokusanmemes/"
-memes = "https://www.instagram.com/memes/"
-hell_yeah_meme67 = "https://www.instagram.com/hell_yeah_meme67/"
-dank_memes_0610 = "https://www.instagram.com/dank_memes_0610/"
-memezar = "https://www.instagram.com/memezar/"
-animemes_taiwan = "https://www.instagram.com/animemes.taiwan/"
-memes_engineering_69 = "https://www.instagram.com/memes_engineering_69/"
-meme_boof = "https://www.instagram.com/meme.boof/"
-hoodentertainmentclub = "https://www.instagram.com/_hoodentertainmentclub_/"
-grad_school_memes = "https://www.instagram.com/grad.school.memes/"
-clerkmeme9487 = "https://www.instagram.com/clerkmeme9487/"
-meme2020 = "https://www.instagram.com/meme2020/"
-memecoding = "https://www.instagram.com/meme_coding/"
-memeviz = "https://www.instagram.com/memeviz/"
-meme_necessities = "https://www.instagram.com/meme_necessities/"
-busimeme = "https://www.instagram.com/busimeme/"
+    RAPID_API_ENDPOINT = "https://instagram47.p.rapidapi.com/user_posts"
+    RAPID_API_KEY = "0c7074abe6msh8f13c4bd42a83abp1c441djsnd0657cbc3cfe"
+    RAPID_API_HOST = "instagram47.p.rapidapi.com" 
 
-# meme hashtags
-meme1 = "https://www.instagram.com/explore/tags/memes😂/"
-meme2 = "https://www.instagram.com/explore/tags/memes/"
-meme3 = "https://www.instagram.com/explore/tags/meme/"
-meme4 = "https://www.instagram.com/explore/tags/dankmemes/"
-meme5 = "https://www.instagram.com/explore/tags/memesdaily/"
-meme6 = "https://www.instagram.com/explore/tags/memestagram/"
-meme7 = "https://www.instagram.com/explore/tags/memer/"
-meme8 = "https://www.instagram.com/explore/tags/memegod/"
-
-# add to list
-meme_page_library = [everday_a_meme,dank_meme_therapy,gokusanmemes,memes,hell_yeah_meme67,dank_memes_0610,memezar,animemes_taiwan,memes_engineering_69,meme_boof,hoodentertainmentclub,grad_school_memes,clerkmeme9487,meme2020,memecoding,memeviz,meme_necessities,busimeme]
-meme_hashtag_library = [meme1,meme2,meme3,meme4,meme5,meme6,meme7,meme8]
+    def __init__(self, username):
+        self.items = self.preprocess(self.fetch(username))   # list of items that are fetched
 
 
-def get_url(url):   
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print("Access approved...")
-            return response.text
-        else:    
-            print(response.status_code)        
-    except Exception as e:
-        print(e)
+    def fetch(self, user_name):
+        try:
+            user_name = user_name.strip()   # just strip front and end whitespaces in case
+            print(f"Using rapid API to send requests regarding Instagram account: {user_name}...")
+            querystring = {"username": user_name}
+            headers = {
+                'x-rapidapi-key': self.RAPID_API_KEY,
+                'x-rapidapi-host': self.RAPID_API_HOST 
+                }
+            response = requests.request("GET", self.RAPID_API_ENDPOINT, headers=headers, params=querystring)
+            return json.loads(response.text)
+        except:
+            print("Unfortunately there occurred an error..")
+            return {}    
+        
+    def preprocess(self, response_dict):
+        if 'status' in response_dict and response_dict['status'] == "Success":
+            print("Status of response is success, we are good to go..")
+            return response_dict['body']['items']
+        
+        print('Status of response is not success')
+        return None
+    
+    def getVideoUrlFromItem(self, item):
+        if 'video_versions' in item:
+            # we have a video post
+            vids = item['video_versions']    
+            if len(vids) >= 1:
+                vid_url = vids[0]['url']
+                return vid_url
+        return ''
+
+    def getImageUrlFromItem(self, item):
+        if 'image_versions2' in item:
+            images = item['image_versions2']
+            if len(images) >= 1:
+                image_url = images['candidates'][0]['url']
+                return image_url
+        return ''
+
+    def getAllUrls(self):
+        if self.items is not None:
+            video_urls = list(map(lambda x: self.getVideoUrlFromItem(x), self.items))
+            image_urls = list(map(lambda x: self.getImageUrlFromItem(x), self.items))
+            # reduce
+            return self.cleanUrls(video_urls) + self.cleanUrls(image_urls)
+        
+        # if is None shouldn't be here
+        print("Fetching failed, plz try another url... ")
         return None
 
-
-def get_posts(url,status):
-    print("Executing....")
-    html = get_url(url)
-    if type(html) == str:
-        selector = etree.HTML(html)
-        links = selector.xpath("//script[starts-with(.,'window._sharedData')]/text()")[0]
-        json_string = links.strip().split("= ")[1][:-1]
-        data = json.loads(json_string)
-
-        if status == "profile":
-            edges = data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']['edges']
-        elif status == "hashtag":
-            edges = data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_top_posts']['edges']
-        url = []
-        for edge in edges:
-            display_url = edge['node']['display_url']
-            print(display_url)
-            url.append(display_url)
-    return url
+    def cleanUrls(self,urls):
+        urls_result = []
+        for url in urls:
+            if url != '':
+                urls_result.append(url)
+        return urls_result
 
 
-# cronjob for everyday
-# add try except later
+def crawlAllAccounts(listOfAccounts):
+    allUrls = []
+    for account in listOfAccounts:
+        posts = getPosts(account)
+        posts_urls = posts.getAllUrls()
+        if posts_urls != None:
+            allUrls += posts_urls
+        print("Sleeping for 10 seconds")
+        time.sleep(10)   # slow down
+    print("Finished crawling all accounts...")
+    return allUrls
 
-current_urls = []
-for mp in meme_page_library:
-    current_urls += get_posts(mp,"profile")
-for mhl in meme_hashtag_library:
-    current_urls += get_posts(mhl,"hashtag")
 
-current_urls = pd.DataFrame({"meme_url": current_urls})
 
-# merging
-existing_urls = pd.read_csv("urls.csv")
-current_urls = pd.concat([current_urls.existing_urls])
-current_urls = pd.DataFrame({'meme_url': current_urls['meme_url'].unique()})
+# url_list: list of urls, save to csv file
+def saveToCsv(url_list):
+    # get datetime
+    now = datetime.datetime.now()
+    timestamp = f"{now.year}-{now.month}-{now.day}" 
+    df = pd.DataFrame({'date': timestamp, 'meme_url': url_list})
+    # concat
+    if os.path.exists('urls.csv'):
+        original_df = pd.read_csv('urls.csv')
+        frames = [original_df, current_df]
+        print("Concatenating old df with new df...")
+        df = pd.concat(frames)
+        print("Dropping duplicates...")
+        df = df.drop_duplicates(subset = ["meme_url"])
+        print('Writing to csv file...')
+        df.to_csv("urls.csv", index=False, sep=",")
+    else:
+        df.to_csv("urls.csv", index=False, sep=",")
+    
 
-# outputting and saving
-current_urls.to_csv("urls.csv",index=False , sep=",")
 
+# whole flow
+# specify all username of account here
+meme_list = ['everyday.a.meme', 'dank_meme_therapy', 'gokusanmemes', 'grad.school.memes','memes_engineering_69', 'sarcastic_lines', 'meme_coding', 'memeviz','epicfunnypage','fuckjerry', 'sarcasm_only', 'daquan', 'thefatjewish', 'lmao', 'societyfeelings', 'couplesnote', 'funnymemes', 'ladbible','hell_yeah_meme67', 'animemes_taiwan']  
+# for test only
+# meme_list = ['epicfunnypage','fuckjerry', 'sarcasm_only']
+allUrls = crawlAllAccounts(meme_list)
+saveToCsv(allUrls)
 
 
 
